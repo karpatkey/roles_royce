dc70ce2f5058540f2206a595fcbfd034fafdff17
Santiago Piccinini <san@ektocomms.space>
2023-06-12 14:25:46 -0300

    refactor: use encodeABI in Method

--- roles_royce/protocols/base.py
+++ roles_royce/protocols/base.py
@@ -1,11 +1,15 @@
+import json
 from roles_royce import Operation
 from roles_royce.utils import to_data_input
+from web3 import Web3
 
 AvatarSafeAddress = object()
 Address = str
 
+
 class InvalidArgument(Exception):
     pass
+
 
 class Method:
     name = None
@@ -13,43 +17,31 @@ class Method:
     fixed_arguments = dict()
     target_address = None
     avatar = None
+    values_dict = None
 
     def __init__(self):
         pass
 
-    def get_args_list(self):
-        return [self.get_arg_value(e) for e in self.signature]
-
-    def get_arg_value(self, element):
-        arg_name, arg_type = element
-        if type(arg_type) in (list, tuple):
-            value = tuple(self.get_arg_value(e) for e in arg_type)
-        else:
-            if arg_name in self.fixed_arguments:
-                value = self.fixed_arguments[arg_name]
-                if value is AvatarSafeAddress:
-                    value = self.avatar
-            else:
-                value = getattr(self, arg_name)
-        if type(arg_type) is str and arg_type.startswith("byte") and type(value) is str:
-            value = bytes.fromhex(value[2:])
-        return value
+    @property
+    def args_list(self):
+        return [self._get_arg_value(e) for e in self.signature]
 
     @property
     def data(self):
-        return to_data_input(self.name, self.short_signature, self.get_args_list())
+        contract = Web3().eth.contract(address=None, abi=self.abi)
+        result = contract.encodeABI(fn_name=self.name, args=self.args_list)
+        return result
 
     @property
     def short_signature(self):
-        return "(" + ",".join([self.get_arg_type(e) for e in self.signature]) + ")"
+        types = ",".join([self._get_arg_type(e) for e in self.signature])
+        return f"{self.name}({types})"
 
-    def get_arg_type(self, element):
-        _, _type = element
-        if type(_type) in (list, tuple):
-            value = "(" + ",".join([self.get_arg_type(e) for e in _type]) + ")"
-        else:
-            value = _type
-        return value
+    @property
+    def abi(self):
+        inputs = [self._abi_for(e) for e in self.signature]
+        abi = {"name": self.name, "type": "function", "inputs": inputs}
+        return json.dumps([abi])
 
     @property
     def contract_address(self):
@@ -58,3 +50,37 @@ class Method:
     @property
     def operation(self):
         return Operation.CALL
+
+    def _get_arg_value(self, element):
+        arg_name, arg_type = element
+        if type(arg_type) in (list, tuple):
+            value = tuple(self._get_arg_value(e) for e in arg_type)
+        else:
+            if arg_name in self.fixed_arguments:
+                print(arg_name)
+                value = self.fixed_arguments[arg_name]
+                if value is AvatarSafeAddress:
+                    value = self.avatar
+            else:
+                value = getattr(self, arg_name)
+        return value
+
+    def _abi_for(self, element):
+        name, _type = element
+        if type(_type) in (list, tuple):
+            value = {"name": name,
+                     "type": "tuple",
+                     "components": [self._abi_for(e) for e in _type]
+                     }
+        else:
+            value = {"name": name, "type": _type}
+        return value
+
+    def _get_arg_type(self, element):
+        _type = element[1]
+        if type(_type) in (list, tuple):
+            types = ",".join([self._get_arg_type(e) for e in _type])
+            value = f"({types})"
+        else:
+            value = _type
+        return value
