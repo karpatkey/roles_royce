42dba93d29fd1edcce74edb5b126565363b96d91
Santiago Piccinini <san@ektocomms.space>
2023-06-13 18:21:26 -0300

    lido tests working

--- tests/test_lido.py
+++ tests/test_lido.py
@@ -1,4 +1,4 @@
-from roles_royce import check, Chain
+from roles_royce import check, send, Chain
 from roles_royce.protocols.eth import lido
 from roles_royce.constants import ETHAddr
 from .utils import web3_eth
@@ -6,34 +6,36 @@ from .utils import web3_eth
 # Test safe
 AVATAR = "0xC01318baB7ee1f5ba734172bF7718b5DC6Ec90E1"
 ROLES_MOD_ADDRESS = "0x1ffAdc16726dd4F91fF275b4bF50651801B06a86"
-MANAGER = "0x216071B1B5681D67A75f7eEAF92CEC8262bE29f7"  # Role=1
+MANAGER = "0x216071B1B5681D67A75f7eEAF92CEC8262bE29f7"
+ROLE = 1
+
 
 def test_methods(web3_eth):
     approve = lido.Approve(amount=100)
-    status = check([approve], role=1, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
+    status = check([approve], role=ROLE, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
                    blockchain=Chain.ETHEREUM, web3=web3_eth, block=17067157)
     assert status
+
 
 def test_deposit(web3_eth):
     deposit = lido.Deposit(eth_amount=10)
     assert deposit.value == 10
     assert deposit.target_address == ETHAddr.stETH
-    status = check([deposit], role=1, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
+    status = check([deposit], role=ROLE, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
                    blockchain=Chain.ETHEREUM, web3=web3_eth, block=17067157)
     assert status
 
-def test_unwrap(web3_eth):
-    unwrap = lido.Unwrap(amount=100)
-    status = check([unwrap], role=1, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
-                   blockchain=Chain.ETHEREUM, web3=web3_eth, block=17067157)
-    assert status
 
 def test_wrap(web3_eth):
-    # ENS preset
-    AVATAR = "0x4F2083f5fBede34C2714aFfb3105539775f7FE64"
-    ROLES_MOD_ADDRESS = "0xf20325cf84b72e8BBF8D8984B8f0059B984B390B"
-    MANAGER = "0xb423e0f6E7430fa29500c5cC9bd83D28c8BD8978"
+    approve = lido.Approve(amount=100)
     wrap = lido.Wrap(amount=10)
-    status = check([wrap], role=1, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
+    status = check([approve, wrap], role=ROLE, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
+                   blockchain=Chain.ETHEREUM, web3=web3_eth, block=17067157)
+    assert status
+
+
+def test_unwrap(web3_eth):
+    unwrap = lido.Unwrap(amount=1_000_000_000_000)
+    status = check([unwrap], role=ROLE, account=MANAGER, roles_mod_address=ROLES_MOD_ADDRESS,
                    blockchain=Chain.ETHEREUM, web3=web3_eth, block=17067157)
     assert status
