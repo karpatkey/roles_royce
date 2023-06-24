6643a3bbf14e4cae886cdffc653f228569fa595d
Richymaestro <xxxx@yyy.com>
2023-06-24 07:56:28 +0200

    feat: added undelegating mkr and fixed comments

--- roles_royce/protocols/eth/makerdao.py
+++ roles_royce/protocols/eth/makerdao.py
@@ -15,6 +15,10 @@ class ApproveWstETH(ApproveDAI):
     """approve stETH with CDP manager as spender"""
     target_address = ETHAddr.wstETH
 
+class ApproveIOU(ApproveDAI):
+    """approve IOU to get MKR undelegated"""
+    target_address = ETHAddr.MakerIOU
+
 class WithdrawCollateral(Method):
     """withdraw collateral from vault"""
     name = "freeGem"
@@ -28,27 +32,39 @@ class WithdrawCollateral(Method):
         self.cdp_vault = cdp_vault
         self.amount = amount
 
-class DepositCollateral(WithdrawCollateral):
+class DepositCollateral(Method):
     """deposit collateral in vault"""
     name = "lockGem"
     in_signature = [("cdp_manager", "address"), ("adapter", "address"),
                     ("cdp_vault", "uint256"), ("amount", "uint256"),
                     ("transfer_from", "bool")]
     fixed_arguments = {"cdp_manager": ETHAddr.MakerCDPManager, "transfer_from": True}
+    target_address = ETHAddr.MakerProxy
+
+    def __init__(self, adapter: Address, cdp_vault: int, amount: int):
+        self.adapter = adapter
+        self.cdp_vault = cdp_vault
+        self.amount = amount
  
 class PayBack(WithdrawCollateral):
     """pay back borrowed"""
     name = "wipe"
 
-class MintDAI(WithdrawCollateral):
+class MintDAI(Method):
     """mint DAI to borrow"""
     name = "draw"
     in_signature = [("cdp_manager", "address"), ("jug", "address"),
                     ("adapter", "address"), ("cdp_vault", "uint256"),
                     ("amount", "uint256")]
     fixed_arguments = {"cdp_manager": ETHAddr.MakerCDPManager, "jug": ETHAddr.MakerJug} 
+    target_address = ETHAddr.MakerProxy
+
+    def __init__(self, adapter: Address, cdp_vault: int, amount: int):
+        self.adapter = adapter
+        self.cdp_vault = cdp_vault
+        self.amount = amount
     
-class PayBackAndWithdrawCol(WithdrawCollateral):
+class PayBackAndWithdrawCol(Method):
     """be free"""
     name = "wipeAllAndFreeGem"
     in_signature = [("cdp_manager", "address"), ("adapter", "address"),
@@ -58,6 +74,16 @@ class PayBackAndWithdrawCol(WithdrawCollateral):
     target_address = ETHAddr.MakerProxy  
 
     def __init__(self, adapter: Address, dai_join: Address, cdp_vault: int, amount: int):
-        super().__init__(adapter, cdp_vault, amount)
+        self.adapter = adapter
+        self.cdp_vault = cdp_vault
+        self.amount = amount
         self.dai_join = dai_join
+
+class UndelegateMKR(Method):
+    """undelegate any MKR"""
+    name = "free"
+    in_signature = [("amount", "uint256")]
+
+    def __init__(self, amount: int):
+        self.amount = amount
    
