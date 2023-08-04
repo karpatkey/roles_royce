7fb77c1624d31ad1c85293f580354196cf1cc988
sgzerbo <santiago@karpatkey.com>
2023-08-04 13:07:35 -0300

    Refactoring

--- roles_royce/toolshed/protocol_utils/spark/__init__.py
+++ roles_royce/toolshed/protocol_utils/spark/__init__.py
@@ -1,6 +1,7 @@
+import web3.eth
 from web3 import Web3
 from decimal import Decimal
-from .addresses_and_abis import addresses, maker_pot_abi
+from .addresses_and_abis import addresses, maker_pot_abi, protocol_data_provider_abi
 from dataclasses import dataclass
 from roles_royce.constants import Chain
 
@@ -10,8 +11,8 @@ class SparkUtils:
     w3: Web3
 
     @staticmethod
-    def get_chi(w3: Web3, blockchain: Chain, block: int | str = 'latest') -> int:
-        maker_pot_contract = w3.eth.contract(address=addresses[blockchain]['maker_pot'], abi=maker_pot_abi)
+    def get_chi(w3: Web3, block: int | str = 'latest') -> int:
+        maker_pot_contract = w3.eth.contract(address=addresses[Chain.get_blockchain_by_chain_id(w3.eth.chain_id)]['maker_pot'], abi=maker_pot_abi)
         ts = w3.eth.get_block(block)['timestamp']
         rho = maker_pot_contract.functions.rho().call(block_identifier=block)
         if ts > rho:
@@ -19,3 +20,16 @@ class SparkUtils:
         else:
             chi = maker_pot_contract.functions.chi().call(block_identifier=block)
         return chi
+    @staticmethod
+    def get_spark_token_addresses(self) -> list[dict]:
+        blockchain = Chain.get_blockchain_by_chain_id(self.w3.eth.chain_id)
+        protocol_data_provider_contract = self.w3.eth.contract(address=addresses[blockchain]['protocol_data_provider'],
+                                                               abi=protocol_data_provider_abi)
+        reserve_tokens = protocol_data_provider_contract.functions.getAllReservesTokens().call(block_identifier=self.block)
+        spark_tokens = []
+        for token in reserve_tokens:
+            data = protocol_data_provider_contract.functions.getReserveTokensAddresses(token[1]).call(block_identifier=self.block)
+            spark_tokens.append(
+                {"underlying": token[1], "interest bearing": data[0], "stable debt": data[1], "variable debt": data[2]}
+            )
+        return spark_tokens
--- roles_royce/toolshed/protocol_utils/spark/addresses_and_abis.py
+++ roles_royce/toolshed/protocol_utils/spark/addresses_and_abis.py
@@ -2,7 +2,9 @@ from roles_royce.constants import Chain
 from roles_royce.abis_utils import load_abi
 addresses = {
     Chain.ETHEREUM: {
+        "protocol_data_provider": "0xFc21d6d146E6086B8359705C8b28512a983db0cb",
         "maker_pot": "0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7"
     }
 }
+protocol_data_provider_abi = load_abi('protocol_data_provider.json')
 maker_pot_abi = load_abi('maker_pot.json')
