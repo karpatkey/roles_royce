22eb95951bb2a3ae110fb6e6c07fa640d059b1bb
dharmendra kariya <dharamendra.kariya@gmail.com>
2023-08-28 19:09:13 +0530

    Minir fix in abi

--- roles_royce/abi_utils.py
+++ roles_royce/abi_utils.py
@@ -9,7 +9,7 @@ def load_abi(abi_filename):
     caller_frame = inspect.stack()[1]
     caller_filename = caller_frame[1]
     caller_file_directory = os.path.dirname(caller_filename)
-    file_path = os.path.join(caller_file_directory, './abis', abi_filename)
+    file_path = os.path.join(caller_file_directory, '.', abi_filename)
     with open(file_path) as f:
         return json.load(f)
 
