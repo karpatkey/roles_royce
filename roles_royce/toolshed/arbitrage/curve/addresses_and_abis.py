d1f91654eb981ef1df836def889af199a592f4cd
dharmendra kariya <dharamendra.kariya@gmail.com>
2023-08-28 22:41:29 +0530

    fix path

--- roles_royce/toolshed/arbitrage/curve/addresses_and_abis.py
+++ roles_royce/toolshed/arbitrage/curve/addresses_and_abis.py
@@ -4,7 +4,7 @@ from roles_royce.abi_utils import load_abi, AddressOrAbi
 
 class GnosisChainAddressesAndAbis:
     DepositZap = AddressOrAbi(address='0xE3FFF29d4DC930EBb787FeCd49Ee5963DADf60b6',
-                                   abi=load_abi('abis/curve_zap_gnosis_chain_abi.json'),
+                                   abi=load_abi('curve_zap_gnosis_chain_abi.json'),
                                    name='deposit_zap_gnosis_chain')
 
 
