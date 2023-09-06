ce863494c9c7c9b5254150635b2f9a745f2d3135
Santiago Piccinini <san@ektocomms.space>
2023-09-06 13:51:43 -0300

    Fix the double enumeration in the docs and refactor

--- docs/_ext/autodoc_method.py
+++ docs/_ext/autodoc_method.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from roles_royce.protocols.base import ContractMethod
 from roles_royce.constants import StrEnum
 from typing import Any
@@ -45,75 +43,62 @@ class ContractMethodDocumenter(ClassDocumenter):
         self.add_line(f"**fixed_arguments**: {method_object.fixed_arguments}", source_name)
 
 
-class IntEnumDocumenter(ClassDocumenter):
-    objtype = 'intenum'
-    directivetype = ClassDocumenter.objtype
+class EnumDocumenter(ClassDocumenter):
     priority = 10 + ClassDocumenter.priority
+    _obj_type = None
+
+    @classmethod
+    def can_document_member(cls,
+                            member: Any, membername: str,
+                            isattr: bool, parent: Any) -> bool:
+        try:
+            return issubclass(member, cls._obj_type)
+        except TypeError:
+            return False
+
+    def get_object_members(self, want_all: bool):
+        members_check_module, members = super().get_object_members(want_all)
+        # filter the enum members so they are not documented twice
+        members = filter(lambda member: not isinstance(member[1], self.object), members)
+        return members_check_module, members
+
+    def add_directive_header(self, sig: str) -> None:
+        super().add_directive_header("")
+
+    def format_member_value(self, value):
+        return value
+
+    def add_content(self, more_content: StringList | None) -> None:
+        super().add_content(more_content)
+        source_name = self.get_sourcename()
+        enum_object = self.object
+        self.add_line('', source_name)
+
+        for the_member_name, enum_member in enum_object.__members__.items():
+            the_member_value = self.format_member_value(enum_member.value)
+
+            self.add_line(f"**{the_member_name}**: {the_member_value}", source_name)
+            self.add_line('', source_name)
+
+
+class IntEnumDocumenter(EnumDocumenter):
+    objtype = 'intenum'
+    _obj_type = IntEnum
     option_spec = dict(ClassDocumenter.option_spec)
+    directivetype = ClassDocumenter.objtype
     option_spec['hex'] = bool_option
 
-    @classmethod
-    def can_document_member(cls,
-                            member: Any, membername: str,
-                            isattr: bool, parent: Any) -> bool:
-        try:
-            return issubclass(member, IntEnum)
-        except TypeError:
-            return False
-
-    def add_directive_header(self, sig: str) -> None:
-        super().add_directive_header("")
-
-    def add_content(self, more_content: StringList | None) -> None:
-
-        super().add_content(more_content)
-
-        source_name = self.get_sourcename()
-        enum_object: IntEnum = self.object
-        use_hex = self.options.hex
-        self.add_line('', source_name)
-
-        for the_member_name, enum_member in enum_object.__members__.items():
-            the_member_value = enum_member.value
-            if use_hex:
-                the_member_value = hex(the_member_value)
-
-            self.add_line(f"**{the_member_name}**: {the_member_value}", source_name)
-            self.add_line('', source_name)
+    def format_member_value(self, value):
+        if self.options.hex:
+            value = hex(value)
+        return value
 
 
-class StrEnumDocumenter(ClassDocumenter):
+class StrEnumDocumenter(EnumDocumenter):
     objtype = 'strenum'
-    directivetype = ClassDocumenter.objtype
-    priority = 10 + ClassDocumenter.priority
+    _obj_type = StrEnum
     option_spec = dict(ClassDocumenter.option_spec)
-
-    @classmethod
-    def can_document_member(cls,
-                            member: Any, membername: str,
-                            isattr: bool, parent: Any) -> bool:
-        try:
-            return issubclass(member, StrEnum)
-        except TypeError:
-            return False
-
-    def add_directive_header(self, sig: str) -> None:
-        super().add_directive_header("")
-
-    def add_content(self, more_content: StringList | None) -> None:
-
-        super().add_content(more_content)
-
-        source_name = self.get_sourcename()
-        enum_object: IntEnum = self.object
-        use_hex = self.options.hex
-        self.add_line('', source_name)
-
-        for the_member_name, enum_member in enum_object.__members__.items():
-            the_member_value = enum_member.value
-            self.add_line(f"**{the_member_name}**: {the_member_value}", source_name)
-            self.add_line('', source_name)
-
+    directivetype = ClassDocumenter.objtype
 
 def setup(app: Sphinx) -> None:
     app.setup_extension('sphinx.ext.autodoc')  # Require autodoc extension
