da69d38737b5afefd06e308f758dc864a30c8994
sgzerbo <santiago@karpatkey.com>
2023-10-13 20:33:35 -0300

    Fixed bug

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from .utils import local_node_gc, accounts, assign_role
 def test_to_hex_32_bytes():
     assert to_hex_32_bytes(15789) == '0x0000000000000000000000000000000000000000000000000000000000003dad'
     assert (to_hex_32_bytes('0x51D34416593a8acF4127dc4a40625A8eFAB9940c') ==
-            '0x00000000000000000000000x51D34416593a8acF4127dc4a40625A8eFAB9940c')
+            '0x00000000000000000000000051D34416593a8acF4127dc4a40625A8eFAB9940c')
 
 
 def test_assign_role(local_node_gc, accounts):
