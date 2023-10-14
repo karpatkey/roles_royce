8f028f11062b9b62a8e58d27e33ca8566feb9a84
sgzerbo <santiago@karpatkey.com>
2023-10-13 22:51:21 -0300

    Improved gauges

--- roles_royce/applications/spark_anti_liquidation_bot/spark_anti_liquidation.py
+++ roles_royce/applications/spark_anti_liquidation_bot/spark_anti_liquidation.py
@@ -2,15 +2,15 @@ from roles_royce.toolshed.anti_liquidation.spark.cdp import SparkCDPManager, CDP
 from web3 import Web3
 from roles_royce.protocols.eth import spark
 from roles_royce.constants import ETHAddr
-from roles_royce.toolshed.protocol_utils.spark.utils import SparkUtils, SparkToken
+from roles_royce.toolshed.protocol_utils.spark.utils import SparkUtils
 from roles_royce.toolshed.alerting.utils import get_tx_receipt_message_with_transfers
 from decimal import Decimal
 from roles_royce import roles
 from roles_royce.evm_utils import erc20_abi
 from roles_royce.toolshed.alerting import SlackMessenger, TelegramMessenger, Messenger, LoggingLevel
-from prometheus_client import start_http_server as prometheus_start_http_server, Gauge, Enum, Info
+from prometheus_client import start_http_server as prometheus_start_http_server
 import logging
-from utils import ENV, log_initial_data, send_status, SchedulerThread
+from utils import ENV, log_initial_data, send_status, SchedulerThread, Gauges
 import time
 from threading import Event
 import sys
@@ -23,18 +23,19 @@ ENV = ENV()
 test_mode = ENV.TEST_MODE
 if test_mode:
     from tests.utils import top_up_address
+
     w3 = Web3(Web3.HTTPProvider(f'http://localhost:{ENV.LOCAL_FORK_PORT}'))
     top_up_address(w3, ENV.BOT_ADDRESS, 1)
 else:
     w3 = Web3(Web3.HTTPProvider(ENV.RPC_ENDPOINT))
 
-#Alert flags
+# Alert flags
 send_status_flag = Event()
 lack_of_gas_warning_flag = Event()
 alerting_health_factor_flag = Event()
 threshold_health_factor_flag = Event()
 
-#Messenger system
+# Messenger system
 slack_messenger = SlackMessenger(webhook=ENV.SLACK_WEBHOOK_URL)
 slack_messenger.start()
 telegram_messenger = TelegramMessenger(bot_token=ENV.TELEGRAM_BOT_TOKEN, chat_id=ENV.TELEGRAM_CHAT_ID)
@@ -47,39 +48,27 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 messenger = Messenger(slack_messenger, telegram_messenger)
 
-#Prometheus metricsfrom prometheus_client import Info
+# Prometheus metricsfrom prometheus_client import Info
 prometheus_start_http_server(ENV.PROMETHEUS_PORT)
-DAI_balances_data = Info('DAI_balances_data', 'DAI token metrics in Spark CDP')
-GNO_balances_data = Info('GNO_balances_data', 'GNO token metrics in Spark CDP')
-health_factor_gauge = Gauge('health_factor', 'Spark CDP health factor')
-is_running_timestamp_gauge = Gauge('is_running_timestamp', 'Updated timestamp to check the bot is running')
-# TODO: This should be generalized for any Spark CDP with any tokens
-GNO_balance_gauge = Gauge('GNO_balance', 'GNO deposited balance of the Spark CDP')
-DAI_borrowed_gauge = Gauge('DAI_borrowed', 'DAI borrowed balance of the Spark CDP')
-GNO_price_gauge = Gauge('GNO_price', 'GNO price')
-DAI_price_gauge = Gauge('DAI_price', 'DAI price')
-bot_ETH_balance_gauge = Gauge('bot_ETH_balance', 'ETH balance of the bot')
-lack_of_gas_warning = Enum('lack_of_gas_warning', 'Bool to track whether warning has already been sent',
-                           states=['True', 'False'])
-lack_of_gas_warning.state('False')
+gauges = Gauges()
 
-#Exception and RPC endpoint failure counters
+# FIXME: there's already an event for this
+lack_of_gas_warning = False
+
+# Exception and RPC endpoint failure counters
 exception_counter = 0
 rpc_endpoint_failure_counter = 0
 
-
 log_initial_data(ENV, messenger)
-
+gauges.alerting_health_factor.set(ENV.ALERTING_HEALTH_FACTOR)
+gauges.health_factor_threshold.set(ENV.THRESHOLD_HEALTH_FACTOR)
 
 # -----------------------------------------------------------------------------------------------------------------------
 
 def bot_do():
     global w3
-    global health_factor_gauge
-    global is_running_timestamp_gauge
-    global GNO_balance_gauge
-    global DAI_borrowed_gauge
-    global bot_ETH_balance_gauge
+    global lack_of_gas_warning
+    global gauges
 
     bot_ETH_balance = w3.eth.get_balance(ENV.BOT_ADDRESS)
     cdp_manager = SparkCDPManager(w3, ENV.AVATAR_SAFE_ADDRESS)
@@ -87,21 +76,20 @@ def bot_do():
 
     if send_status_flag.is_set():
         send_status_flag.clear()
-        send_status(messenger, cdp, bot_ETH_balance/1e18)
+        send_status(messenger, cdp, bot_ETH_balance / 1e18)
 
-    bot_ETH_balance_gauge.set(bot_ETH_balance / 1e18)
-    health_factor_gauge.set(float(cdp.health_factor))
-    is_running_timestamp_gauge.set_to_current_time()
+    gauges.bot_ETH_balance.set(bot_ETH_balance / 1e18)
+    gauges.health_factor.set(float(cdp.health_factor))
     for element in cdp.balances_data:
         if element[CDPData.UnderlyingAddress] == ETHAddr.GNO:
-            GNO_balance_gauge.set(element[CDPData.InterestBearingBalance])
-            GNO_price_gauge.set(element[CDPData.UnderlyingPriceUSD])
+            gauges.GNO_deposited.set(element[CDPData.InterestBearingBalance])
+            gauges.GNO_price.set(element[CDPData.UnderlyingPriceUSD])
         if element[CDPData.UnderlyingAddress] == ETHAddr.DAI:
-            DAI_borrowed_gauge.set(element[CDPData.VariableDebtBalance])
-            DAI_price_gauge.set(element[CDPData.UnderlyingPriceUSD])
+            gauges.DAI_borrowed.set(element[CDPData.VariableDebtBalance])
+            gauges.DAI_price.set(element[CDPData.UnderlyingPriceUSD])
 
-    #DAI_balances_data.info(cdp.balances_data[0])
-    #GNO_balances_data.info(cdp.balances_data[1])
+    # DAI_balances_data.info(cdp.balances_data[0])
+    # GNO_balances_data.info(cdp.balances_data[1])
 
     logger.info(
         f"Target health factor: {ENV.TARGET_HEALTH_FACTOR}, Alerting health factor: {ENV.ALERTING_HEALTH_FACTOR}, "
@@ -156,7 +144,6 @@ def bot_do():
 
             amount_of_sDAI_to_redeem = sDAI_balance
 
-
         logger.info(f'Redeeming {amount_of_sDAI_to_redeem} sDAI for DAI...')
         tx_receipt_sDAI_redeemed = roles.send(
             [spark.RedeemSDAIforDAI(amount=amount_of_sDAI_to_redeem, avatar=ENV.AVATAR_SAFE_ADDRESS)],
@@ -178,7 +165,6 @@ def bot_do():
 
             amount_of_DAI_to_repay = DAI_balance
 
-
         tx_receipt_debt_repayed = cdp_manager.repay_single_token_debt(spark_cdp=cdp, token_in_address=ETHAddr.DAI,
                                                                       token_in_amount=amount_of_DAI_to_repay,
                                                                       rate_model=spark.RateModel.VARIABLE,
@@ -194,32 +180,28 @@ def bot_do():
         messenger.log_and_alert(LoggingLevel.Info, 'DAI debt repayed', message + '\n' + extra_message,
                                 slack_msg=message_slack + '\n' + extra_message)
 
-        bot_ETH_balance_gauge.set(bot_ETH_balance / 1e18)
-        health_factor_gauge.set(float(cdp.health_factor))
-        is_running_timestamp_gauge.set_to_current_time()
+        gauges.bot_ETH_balance.set(bot_ETH_balance / 1e18)
+        gauges.health_factor.set(float(cdp.health_factor))
 
         if bot_ETH_balance < 0.1:
             title = 'Lack of ETH for gas'
             message = 'Im running outta ETH for gas! Only %.5f ETH left.' % (bot_ETH_balance / (10 ** 18))
-            messenger.log_and_alert(LoggingLevel.Warning, title, message, alert_flag=lack_of_gas_warning_flag.is_set())
-            lack_of_gas_warning.state('True')
-            lack_of_gas_warning_flag.set()
+            messenger.log_and_alert(LoggingLevel.Warning, title, message, alert_flag=lack_of_gas_warning)
+            lack_of_gas_warning = True
 
-        if bot_ETH_balance >= 0.1 and lack_of_gas_warning_flag.is_set():
-            lack_of_gas_warning.state('False')
-            lack_of_gas_warning_flag.clear()
+        if bot_ETH_balance >= 0.1 and lack_of_gas_warning_flag:
+            lack_of_gas_warning = False
 
 
 # -----------------------------MAIN LOOP-----------------------------------------
 
-#Status notification scheduling
+# Status notification scheduling
 if ENV.STATUS_NOTIFICATION_HOUR != '':
-    #FIXME: make sure the scheduling job is set at UTC time
+    # FIXME: make sure the scheduling job is set at UTC time
     status_run_time = datetime.time(hour=ENV.STATUS_NOTIFICATION_HOUR, minute=0, second=0)
     schedule.every().day.at(str(status_run_time)).do(lambda: send_status_flag.set())
     scheduler_thread = SchedulerThread()
     scheduler_thread.start()
-
 
 while True:
 
@@ -237,7 +219,9 @@ while True:
                             time.sleep(5)
                             if not w3.is_connected(show_traceback=True):
                                 messenger.log_and_alert(LoggingLevel.Error, title='Error',
-                                                        message=f'  RPC endpoint {ENV.RPC_ENDPOINT} and fallback RPC endpoint {ENV.FALLBACK_RPC_ENDPOINT} are both not working.')
+                                                        message=f'  RPC endpoint {ENV.RPC_ENDPOINT} and fallback RPC '
+                                                                f'endpoint {ENV.FALLBACK_RPC_ENDPOINT} are both not '
+                                                                f'working.')
                                 rpc_endpoint_failure_counter += 1
                                 continue
                     else:
@@ -246,18 +230,19 @@ while True:
                         rpc_endpoint_failure_counter += 1
                         continue
 
-            if rpc_endpoint_failure_counter == 5:#TODO: this can be added as an environment variable
-                messenger.log_and_alert(LoggingLevel.Error, title= 'Too many RPC endpoint failures, exiting...', message='')
-                time.sleep(5) # Cooldown time for the messenger system to send messages in queue
+            if rpc_endpoint_failure_counter == 5:  # TODO: this can be added as an environment variable
+                messenger.log_and_alert(LoggingLevel.Error, title='Too many RPC endpoint failures, exiting...',
+                                        message='')
+                time.sleep(5)  # Cooldown time for the messenger system to send messages in queue
                 sys.exit(1)
 
-        #bot_do()
+        bot_do()
 
     except Exception as e:
-        messenger.log_and_alert(LoggingLevel.Error, title='Exception', message= '  ' + str(e.args[0]))
+        messenger.log_and_alert(LoggingLevel.Error, title='Exception', message='  ' + str(e.args[0]))
         exception_counter += 1
-        if exception_counter == 5:#TODO: this can be added as an environment variable
-            messenger.log_and_alert(LoggingLevel.Error, title= 'Too many exceptions, exiting...', message='')
-            time.sleep(5) # Cooldown time for the messenger system to send messages in queue
+        if exception_counter == 5:  # TODO: this can be added as an environment variable
+            messenger.log_and_alert(LoggingLevel.Error, title='Too many exceptions, exiting...', message='')
+            time.sleep(5)  # Cooldown time for the messenger system to send messages in queue
             sys.exit(1)
     time.sleep(ENV.COOLDOWN_MINUTES * 60)
--- roles_royce/applications/spark_anti_liquidation_bot/utils.py
+++ roles_royce/applications/spark_anti_liquidation_bot/utils.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 import threading
-
+from prometheus_client import Gauge
 import schedule
 import time
 from decouple import config
@@ -94,3 +94,15 @@ class SchedulerThread(threading.Thread):
 
     def stop(self):
         self.running = False
+
+@dataclass
+class Gauges:
+    health_factor = Gauge('health_factor', 'Spark CDP health factor')
+    # TODO: This should be generalized for any Spark CDP with any tokens
+    GNO_deposited = Gauge('GNO_balance', 'GNO deposited balance of the Spark CDP')
+    DAI_borrowed = Gauge('DAI_borrowed', 'DAI borrowed balance of the Spark CDP')
+    GNO_price = Gauge('GNO_price', 'GNO price')
+    DAI_price = Gauge('DAI_price', 'DAI price')
+    bot_ETH_balance = Gauge('bot_ETH_balance', 'ETH balance of the bot')
+    alerting_health_factor = Gauge('alerting_health_factor', 'Alerting health factor')
+    health_factor_threshold = Gauge('health_factor_threshold', 'Health factor threshold')
