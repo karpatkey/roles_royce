54ccb35ca1da034616cc6c9ef0a831cddeff0630
santi <santiago@karpatkey.com>
2023-11-10 21:44:04 -0300

    Refactored config builder

--- roles_royce/applications/panic_button_app/config/config_builder.py
+++ roles_royce/applications/panic_button_app/config/config_builder.py
@@ -4,8 +4,47 @@ from web3 import Web3
 from defabipedia import balancer, aura
 from defabipedia.types import Chains
 from roles_royce.protocols.balancer.utils import Pool, PoolKind
+from roles_royce.constants import StrEnum
+from utils import get_bpt_from_aura, get_tokens_from_bpt
+import os
+from dataclasses import dataclass
 
-def seed_file(file: str, dao: str, blockchain: str) -> None:
+
+# -----------------------------------------------------------------------------------------------------------------------
+# StrEnums
+# -----------------------------------------------------------------------------------------------------------------------
+
+class DAO(StrEnum):
+    GnosisDAO = "GnosisDAO"
+    GnosisLTD = "GnosisLTD"
+
+    def __str__(self):
+        return self.name
+
+
+class Blockchain(StrEnum):
+    Ethereum = "Ethereum"
+    Gnosis = "Gnosis"
+
+    def __str__(self):
+        return self.name
+
+
+class Protocol(StrEnum):
+    Balancer = "Balancer"
+    Aura = "Aura"
+    Lido = "Lido"
+
+    def __str__(self):
+        return self.name
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# General
+# -----------------------------------------------------------------------------------------------------------------------
+
+def seed_file(dao: str, blockchain: str) -> None:
+    file = f"strategies/{dao}{blockchain}.json"
     with open(file, "w") as f:
         data = {
             "dao": dao,
@@ -25,53 +64,25 @@ def seed_file(file: str, dao: str, blockchain: str) -> None:
         }
         json.dump(data, f)
 
-def get_bpt_from_aura(w3: Web3):
-    booster_ctr = aura.ContractSpecs[Chains.get_blockchain_from_web3(w3)].Booster.contract(w3)
-    blockchain = Chains.get_blockchain_from_web3(w3)
-    pool_length = booster_ctr.functions.poolLength().call()
-    result = []
-    for i in range(0,pool_length,1):
-        info = booster_ctr.functions.poolInfo(i).call()
-        info_dict = {"blockchain":blockchain,"bpt_address":info[0],"aura_address":info[3]}
-        if len(result) == 0:
-            result.append(info_dict)
-        if any(d['bpt_address'] == info_dict['bpt_address'] for d in result):
-            for d in result:
-                if d['bpt_address'] == info_dict['bpt_address']:
-                    d['aura_address'] = info_dict['aura_address']
-                    break
-        else:
-            result.append(info_dict)
-    return result
 
-def add_balancer_positions(w3: Web3, template: dict, position_ids: list[str], bpt_addresses: list[Address]):
-    result = []
-    for bpt_address in bpt_addresses:
-        position = template.copy()
-        position["position_id"] = position_ids[bpt_addresses.index(bpt_address)]
-        bpt_address = Web3.to_checksum_address(bpt_address)
-        for i in range(3):
-            position["position_exec_config"][i]["parameters"][0]["value"] = bpt_address
-        bpt_contract = w3.eth.contract(address=bpt_address, abi=balancer.Abis[Chains.get_blockchain_from_web3(w3)].UniversalBPT.abi)
-        pool_id = bpt_contract.functions.getPoolId().call()
-        vault_contract = balancer.ContractSpecs[Chains.get_blockchain_from_web3(w3)].Vault.contract(w3)
-        pool_tokens = vault_contract.functions.getPoolTokens(pool_id).call()[0]
-        pool = Pool(w3=w3, pool_id=pool_id)
-        if pool.pool_kind() == PoolKind.ComposableStablePool:
-            del pool_tokens[pool.bpt_index_from_composable()] # Remove the BPT if it is a composable stable
-        del position["position_exec_config"][1]["parameters"][2]["options"][0]  # Remove the dummy element in template
-        for token_address in pool_tokens:
-            token_contract = w3.eth.contract(address=token_address, abi=balancer.Abis[Chains.get_blockchain_from_web3(w3)].ERC20.abi)
-            token_symbol = token_contract.functions.symbol().call()
-            position["position_exec_config"][1]["parameters"][2]["options"].append({
-                "value": token_address,
-                "label": token_symbol
-            })
-        result.append(position)
-    return result
+# -----------------------------------------------------------------------------------------------------------------------
+# Position classes
+# -----------------------------------------------------------------------------------------------------------------------
+
+@dataclass
+class BalancerPosition:
+    position_id: str
+    bpt_address: Address
+
+@dataclass
+class AuraPosition:
+    position_id: str
+    bpt_address: Address
+
+# -----------------------------------------------------------------------------------------------------------------------
+
 
 def add_aura_positions(w3: Web3, template: dict, position_ids: list[str], bpt_addresses: list[Address]):
-
     aura_balancer_list = get_bpt_from_aura(w3)
     result = []
     for bpt_address in bpt_addresses:
@@ -83,20 +94,23 @@ def add_aura_positions(w3: Web3, template: dict, position_ids: list[str], bpt_ad
                 aura_address = item.get('aura_address')
                 break
         for i in range(4):
-            position["position_exec_config"][i]["parameters"][0]["value"] = aura_address
-    
-        bpt_contract = w3.eth.contract(address=bpt_address, abi=balancer.Abis[Chains.get_blockchain_from_web3(w3)].UniversalBPT.abi)
+            position["exec_config"][i]["parameters"][0]["value"] = aura_address
+
+        bpt_contract = w3.eth.contract(address=bpt_address,
+                                       abi=balancer.Abis[Chains.get_blockchain_from_web3(w3)].UniversalBPT.abi)
         pool_id = bpt_contract.functions.getPoolId().call()
         vault_contract = balancer.ContractSpecs[Chains.get_blockchain_from_web3(w3)].Vault.contract(w3)
         pool_tokens = vault_contract.functions.getPoolTokens(pool_id).call()[0]
         pool = Pool(w3=w3, pool_id=pool_id)
         if pool.pool_kind() == PoolKind.ComposableStablePool:
-            del pool_tokens[pool.bpt_index_from_composable()] # Remove the BPT if it is a composable stable
-        del position["position_exec_config"][2]["parameters"][2]["options"][0]  # Remove the dummy element in template        
+            del pool_tokens[pool.bpt_index_from_composable()]  # Remove the BPT if it is a composable stable
+        del position["exec_config"][2]["parameters"][2]["options"][
+            0]  # Remove the dummy element in template
         for token_address in pool_tokens:
-            token_contract = w3.eth.contract(address=token_address, abi=balancer.Abis[Chains.get_blockchain_from_web3(w3)].ERC20.abi)
+            token_contract = w3.eth.contract(address=token_address,
+                                             abi=balancer.Abis[Chains.get_blockchain_from_web3(w3)].ERC20.abi)
             token_symbol = token_contract.functions.symbol().call()
-            position["position_exec_config"][2]["parameters"][2]["options"].append({
+            position["exec_config"][2]["parameters"][2]["options"].append({
                 "value": token_address,
                 "label": token_symbol
             })
@@ -104,32 +118,61 @@ def add_aura_positions(w3: Web3, template: dict, position_ids: list[str], bpt_ad
     return result
 
 
+
 def add_lido_positions(protocol, template, position_id):
-    template['position_id'] = f"{protocol}_{position_id}" 
-    return template 
+    template['position_id'] = f"{protocol}_{position_id}"
+    return template
 
 
-def add_positions(w3: Web3, file: str, templates_folder: str, protocol: str, position_ids: list[str], addresses: list[Address] = None):
-    with open(file, "r") as f:
-        data = json.load(f)
 
-    if protocol == 'balancer':
-        with open(templates_folder / 'balancer_template.json', 'r') as f:
-            template = json.load(f)
-        result = add_balancer_positions(w3, template, position_ids, addresses)
-        
-    elif protocol == 'aura':
-        with open(templates_folder / 'aura_template.json', 'r') as f:
-            template = json.load(f)
-        result = add_aura_positions(w3, template, position_ids, addresses)
+# -----------------------------------------------------------------------------------------------------------------------
+# Json builder
+# -----------------------------------------------------------------------------------------------------------------------
+@dataclass
+class DAOStrategiesBuilder:
+    dao: DAO
+    blockchain: Blockchain
+    balancer: list[BalancerPosition] | None = None
+    aura: list[AuraPosition] | None = None
+    lido: bool = False  # We either have funds in Lido or we don't
 
-    elif protocol == 'lido':
-        with open(templates_folder / 'lido_template.json', 'r') as f:
-            template = json.load(f)
-        result = add_lido_positions(w3, template, position_ids)
+    def build_json(self, w3: Web3):
+        self.add_balancer_positions(w3, self.balancer)
+        # TODO: add_aura_positions
 
-    data['positions'] = result
-    with open(file, "w") as f:
-        json.dump(data, f)
+    def add_balancer_positions(self, w3: Web3, positions: list[BalancerPosition]):
+        file = f"strategies/{self.dao}{self.blockchain}.json"
 
+        if not os.path.isfile(file):
+            seed_file(self.dao, self.blockchain)
 
+        with open(file, "r") as f:
+            strategies = json.load(f)
+
+        with open('templates/balancer_template.json', 'r') as f:
+            balancer_template = json.load(f)
+
+        for balancer_position in positions:
+            bpt_address = Web3.to_checksum_address(balancer_position.bpt_address)
+            if bpt_address in [item for position in strategies["positions"] for item in position['position_id_tech']]:
+                continue
+            position = balancer_template.copy()
+            del position["exec_config"][1]["parameters"][2]["options"][0]  # Remove the dummy element in template
+
+            position["position_id"] = balancer_position.position_id
+            position["position_id_tech"] = bpt_address
+            for i in range(3):
+                position["exec_config"][i]["parameters"][0]["value"] = bpt_address
+            pool_tokens = get_tokens_from_bpt(w3, bpt_address)
+
+            position["position_id_human_readable"] = 'Balancer'
+            for token in pool_tokens:
+                position["exec_config"][1]["parameters"][2]["options"].append({
+                    "value": token['address'],
+                    "label": token['symbol']
+                })
+                position["position_id_human_readable"] = position["position_id_human_readable"] + f"_{token['symbol']}"
+            strategies['positions'].append(position)
+
+        with open(file, "w") as f:
+            json.dump(strategies, f)
--- roles_royce/applications/panic_button_app/config/dao_config_builder.py
+++ roles_royce/applications/panic_button_app/config/dao_config_builder.py
@@ -1,37 +1,19 @@
-import json
-import os.path
-import pathlib
-
-from web3.types import Address
 from web3 import Web3
+from roles_royce.applications.panic_button_app.config.config_builder import DAOStrategiesBuilder, DAO, Blockchain, \
+    BalancerPosition, AuraPosition
 
-from defabipedia import balancer, aura
-from defabipedia.types import Chains
-from roles_royce.protocols.balancer.utils import Pool, PoolKind
-from roles_royce.applications.panic_button_app.config.config_builder import seed_file, add_positions
 
-def json_builder(dao: str, blockchain: str, w3: Web3, protocol: str, position_ids: list[str], addresses: list[Address], folder_path: str = None):
-    """builds json from lists of addresses
+def main():
+    w3_eth = Web3(Web3.HTTPProvider('https://eth.llamarpc.com'))
 
-    Args:
-        dao (str): give the DAO name
-        blockchain (str): give the blockchain
-        w3 (Web3): endpoint to get data from
-        protocol (str): the protocol which template will be used
-        position_ids (list[str]): name of the positions to add
-        addresses (list[Address]): list of addresses to add (for Balancer and Aura bpt addresses are enough)
-        folder_path (str, optional): folder_path with the DAO files and the templates. Defaults to None.
-    """
-    dao = dao.replace(" ","")
-    if not folder_path:
-        folder_path = pathlib.Path(os.path.dirname(__file__)).resolve().parent / 'config'
-    filename = folder_path / f"strategies{dao}{blockchain}.json"
+    strategies_gnosis_dao_ethereum = DAOStrategiesBuilder(DAO.GnosisDAO,
+                                                          Blockchain.Ethereum,
+                                                          balancer=[BalancerPosition(position_id='test_id_1',
+                                                                                     bpt_address='0xcfca23ca9ca720b6e98e3eb9b6aa0ffc4a5c08b9'),
+                                                                    BalancerPosition(position_id='test_id_2',
+                                                                                     bpt_address='0xde8c195aa41c11a0c4787372defbbddaa31306d2')])
+    strategies_gnosis_dao_ethereum.build_json(w3_eth)
 
-    if os.path.isfile(filename):
-        add_positions(w3, filename, folder_path, protocol, position_ids, addresses)
-        
-    else:
-        seed_file(filename, dao, blockchain)
-        add_positions(w3, filename, folder_path, protocol, position_ids, addresses)
 
-    print('done')
+if __name__ == "__main__":
+    main()
