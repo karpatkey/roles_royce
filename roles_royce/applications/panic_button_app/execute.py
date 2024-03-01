cdadc10f61c573646b39758cc9d2c5de4d24d840
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-01 14:48:36 +0000

    WIP TEMP: don't reset fork so we can have failed simulations
    - This is just to be able to test this scenario in AxA
    - Will delete this branch after

--- roles_royce/applications/panic_button_app/execute.py
+++ roles_royce/applications/panic_button_app/execute.py
@@ -1,17 +1,36 @@
 import argparse
-from roles_royce.applications.panic_button_app.utils import ENV, start_the_engine, fork_reset
+from roles_royce.applications.panic_button_app.utils import (
+    ENV,
+    start_the_engine,
+    fork_reset,
+)
 import json
-from roles_royce.roles_modifier import update_gas_fees_parameters_and_nonce, set_gas_strategy, GasStrategies
+from roles_royce.roles_modifier import (
+    update_gas_fees_parameters_and_nonce,
+    set_gas_strategy,
+    GasStrategies,
+)
 
 
 def main():
     try:
-        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
 
-        parser.add_argument("-d", "--dao", type=str, help="DAO whose funds are to be removed.")
-        parser.add_argument("-b", "--blockchain", type=str, help="Blockchain where the funds are deposited.")
+        parser.add_argument(
+            "-d", "--dao", type=str, help="DAO whose funds are to be removed."
+        )
+        parser.add_argument(
+            "-b",
+            "--blockchain",
+            type=str,
+            help="Blockchain where the funds are deposited.",
+        )
 
-        parser.add_argument("-t", "--transaction", type=str, help="Transaction json to execute.")
+        parser.add_argument(
+            "-t", "--transaction", type=str, help="Transaction json to execute."
+        )
 
         args = parser.parse_args()
         tx = json.loads(args.transaction)
@@ -21,13 +40,15 @@ def main():
         set_gas_strategy(GasStrategies.AGGRESIVE)
         tx = update_gas_fees_parameters_and_nonce(w3, tx)
 
-        if env.MODE == 'production':  # In production environment, send the transaction to the real blockchain
+        if (
+            env.MODE == "production"
+        ):  # In production environment, send the transaction to the real blockchain
             signed = w3.eth.account.sign_transaction(tx, env.PRIVATE_KEY)
             tx_hash = w3_MEV.eth.send_raw_transaction(signed.rawTransaction)
         else:  # In development environment, send the transaction to the local fork with the unlocked account
             tx_hash = w3.eth.send_transaction(tx)
             w3.eth.wait_for_transaction_receipt(tx_hash)
-            reset = fork_reset(w3, w3.manager.provider.endpoint_uri)
+            # reset = fork_reset(w3, w3.manager.provider.endpoint_uri)
         response_message = {"status": 200, "tx_hash": tx_hash.hex()}
     except Exception as e:
         response_message = {"status": 500, "message": f"Error: {e}"}
