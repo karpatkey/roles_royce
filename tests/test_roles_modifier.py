a8979b4af4600666bf3d4537ca877c664e0e50ae
Richymaestro <xxxx@yyy.com>
2024-03-06 16:23:18 +0000

    tests_fix2

--- tests/test_roles_modifier.py
+++ tests/test_roles_modifier.py
@@ -29,6 +29,7 @@ class RolesModTester(RolesMod):
 
 def test_check_and_execute(local_node_gc):
     w3 = local_node_gc.w3
+    local_node_gc.reset_state()
     local_node_gc.set_block(TEST_BLOCK)
     usdt_approve = "0x095ea7b30000000000000000000000007f90122bf0700f9e7e1f688fe926940e8839f35300000000000000000000000000000000000000000000000000000000000003e8"
     roles = RolesModTester(role=ROLE, contract_address="0xB6CeDb9603e7992A5d42ea2246B3ba0a21342503", web3=w3,
@@ -65,6 +66,7 @@ def test_gas_limit_estimation(local_node_gc):
 
 def test_gas_strategy(local_node_gc):
     w3 = local_node_gc.w3
+    local_node_gc.reset_state()
     local_node_gc.set_block(TEST_BLOCK)
     usdt_approve = "0x095ea7b30000000000000000000000007f90122bf0700f9e7e1f688fe926940e8839f35300000000000000000000000000000000000000000000000000000000000003e8"
 
