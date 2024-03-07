5be517172be17753620e16a2ee8978e282a50143
santi <santiago@karpatkey.com>
2024-03-07 20:28:59 -0300

    Polished amount calculations

--- roles_royce/protocols/uniswap_v3/utils.py
+++ roles_royce/protocols/uniswap_v3/utils.py
@@ -9,68 +9,72 @@ from defabipedia.types import Chain
 from defabipedia.tokens import EthereumTokenAddr as ETHAddr
 
 
-def validate_tokens(token0: Address, token1: Address):
+def validate_tokens(token0: Address, token1: Address) -> bool:
     if token0 == token1:
         raise ValueError("token0 and token1 must be different")
+    else:
+        return True
 
 
-def validate_amounts(
-    amount0_desired: int | None, amount1_desired: int | None
-) -> (int | None, int | None):
+def validate_amounts(amount0_desired: int | None, amount1_desired: int | None) -> bool:
     if not amount0_desired and not amount1_desired:
         raise ValueError("Either amount0_desired or amount1_desired must be provided")
 
+    if amount0_desired and amount1_desired:
+        raise ValueError("Only one amount can be provided")
     if amount0_desired:
         if amount0_desired <= 0:
             raise ValueError("amount0_desired must be greater than 0")
-        else:
-            if amount1_desired:
-                raise ValueError("Only one amount can be provided")
-
-    else:
+    elif amount1_desired:
         if amount1_desired <= 0:
             raise ValueError("amount1_desired must be greater than 0")
-        else:
-            if amount0_desired:
-                raise ValueError("Only one amount can be provided")
 
-    return amount0_desired, amount1_desired
+    return True
 
 
-def validate_price_range(
-    token1_min_price: float, token1_max_price: float, pool_price: float
-) -> None:
-    if not token1_min_price > 0:
+def validate_price_range(token0_min_price: float, token0_max_price: float, pool_price: float) -> bool:
+    if not token0_min_price > 0:
         raise ValueError("token0_min_price must be greater than 0")
 
-    if not token1_max_price > 0:
+    if not token0_max_price > 0:
         raise ValueError("token0_max_price must be greater than 0")
 
-    if not (token1_min_price < pool_price < token1_max_price):
+    if not (token0_min_price < pool_price < token0_max_price):
         raise ValueError("pool_price out of range")
 
+    return True
 
-def validate_slippage(amount0_min_slippage: float, amount1_min_slippage: float) -> None:
+
+def validate_slippage(amount0_min_slippage: float, amount1_min_slippage: float) -> bool:
     if not 0 <= amount0_min_slippage <= 100:
         raise ValueError("amount0_min_slippage must be between 0 and 100")
 
     if not 0 <= amount1_min_slippage <= 100:
         raise ValueError("amount1_min_slippage must be between 0 and 100")
 
+    return True
 
-def validate_removed_liquidity_percentage(removed_liquidity_percentage: float) -> None:
+
+def validate_removed_liquidity_percentage(removed_liquidity_percentage: float) -> bool:
     if not 0 <= removed_liquidity_percentage <= 100:
         raise ValueError("removed_liquidity_percentage must be between 0 and 100")
+    return True
 
 
-def check_allowance(
-    w3: Web3, owner: Address, spender: Address, token: Address, amount: Decimal
-) -> bool:
-    allowance = (
-        w3.eth.contract(address=token, abi=TokenAbis.ERC20.abi)
-        .functions.allowance(owner, spender)
-        .call()
-    )
+def check_allowance(w3: Web3, owner: Address, spender: Address, token: Address, amount: int) -> bool:
+    """Check if the owner has enough allowance for the spender to spend the amount of tokens.
+
+    Args:
+        w3: Web3 instance
+        owner: Address of the owner
+        spender: Address of the spender
+        token: Address of the token
+        amount: Amount of tokens to spend
+
+    Returns:
+        bool: True if the owner has enough allowance, False otherwise
+    """
+    allowance = w3.eth.contract(address=token, abi=TokenAbis.ERC20.abi).functions.allowance(owner, spender).call()
     return allowance >= amount
 
 
@@ -107,7 +111,7 @@ class Pool:
             address=self.addr, abi=Abis[blockchain].Pool.abi
         )
         self.sqrt_price_x96, self.ic = self.pool_contract.functions.slot0().call()[0:2]
-        self.sqrt_price = Decimal(self.sqrt_price_x96) / Decimal(2**96)
+        self.sqrt_price = Decimal(self.sqrt_price_x96) / Decimal(2 ** 96)
         self.token0 = self.pool_contract.functions.token0().call()
         self.token0_decimals = (
             w3.eth.contract(address=self.token0, abi=TokenAbis.ERC20.abi)
@@ -121,7 +125,7 @@ class Pool:
             .call()
         )
 
-        self.price = (self.sqrt_price**2) / Decimal(
+        self.price = (self.sqrt_price ** 2) / Decimal(
             10 ** (self.token1_decimals - self.token0_decimals)
         )
         self.tick_spacing = self.pool_contract.functions.tickSpacing().call()
@@ -169,9 +173,9 @@ class NFTPosition:
                 amount1 = self.liquidity * (sb - sa)
             elif self.tick_lower < self.pool.ic < self.tick_upper:
                 amount0 = (
-                    self.liquidity
-                    * (sb - self.pool.sqrt_price)
-                    / (self.pool.sqrt_price * sb)
+                        self.liquidity
+                        * (sb - self.pool.sqrt_price)
+                        / (self.pool.sqrt_price * sb)
                 )
                 amount1 = self.liquidity * (self.pool.sqrt_price - sa)
             else:
@@ -189,14 +193,14 @@ class NFTPosition:
 
 
 def set_and_check_desired_amounts(
-    w3: Web3,
-    owner: Address,
-    amount0_desired: int | None,
-    amount1_desired: int | None,
-    pool: Pool,
-    tick_lower: int,
-    tick_upper: int,
-    send_eth: bool,
+        w3: Web3,
+        owner: Address,
+        amount0_desired: int | None,
+        amount1_desired: int | None,
+        pool: Pool,
+        tick_lower: int,
+        tick_upper: int,
+        send_eth: bool,
 ) -> (int, int):
     """
     Returns the amounts of tokens desired to deposit, calculating one amount from the other. If amount_0_desired is
@@ -220,35 +224,23 @@ def set_and_check_desired_amounts(
     Raises:
         ValueError: If there is not enough balance of either token
     """
+    sqrt_price_x96_decimal = Decimal(pool.sqrt_price_x96)
+    price_min = Decimal(1.0001) ** Decimal(tick_lower / 2)
+    price_max = Decimal(1.0001) ** Decimal(tick_upper / 2)
     if not amount0_desired and not amount1_desired:
         raise ValueError("Either amount0_desired or amount1_desired must be provided")
     elif amount0_desired and amount1_desired:
         raise ValueError("Only one amount can be provided")
     elif amount0_desired and (not amount1_desired):
         amount1_desired = (
-            Decimal(amount0_desired)
-            * Decimal(
-                pool.sqrt_price_x96
-                * 1.0001 ** (tick_upper / 2)
-                * (pool.sqrt_price_x96 - 1.0001 ** (tick_lower / 2) * (2**96))
-            )
-            / Decimal(
-                (2**96) * (1.0001 ** (tick_upper / 2) * (2**96) - pool.sqrt_price_x96)
-            )
+                Decimal(amount0_desired) * sqrt_price_x96_decimal * price_max
+                * (sqrt_price_x96_decimal - price_min * Decimal(2 ** 96))
+                / Decimal(2 ** 96) / (price_max * Decimal(2 ** 96) - sqrt_price_x96_decimal)
         )
     else:
         amount0_desired = (
-            Decimal(amount1_desired)
-            * Decimal(
-                (2**96) * (1.0001 ** (tick_upper / 2) * (2**96) - pool.sqrt_price_x96)
-            )
-            / (
-                Decimal(
-                    pool.sqrt_price_x96
-                    * 1.0001 ** (tick_upper / 2)
-                    * (pool.sqrt_price_x96 - 1.0001 ** (tick_lower / 2) * (2**96))
-                )
-            )
+                Decimal(amount1_desired) * (Decimal(2 ** 96) * (price_max * Decimal(2 ** 96) - sqrt_price_x96_decimal))
+                / (sqrt_price_x96_decimal * price_max * (sqrt_price_x96_decimal - price_min * Decimal(2 ** 96)))
         )
     if send_eth:
         if pool.token0 != ETHAddr.WETH and pool.token1 != ETHAddr.WETH:
