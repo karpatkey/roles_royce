08111e679b052759127ba9c3cd7d2c9b8960b6eb
santi <santiago@karpatkey.com>
2024-03-08 00:58:04 -0300

    Updated docker file and env file

--- roles_royce/applications/run_bot.sh
+++ roles_royce/applications/run_bot.sh
@@ -10,6 +10,8 @@ elif [[ "$BOT_NAME" == "EURe_rebalancing" ]]; then
   python3 -u "$SCRIPT_DIR/EURe_rebalancing_bot/EURe_bot.py"
 elif [[ "$BOT_NAME" == "spark_anti_liquidation" ]]; then
   python3 -u "$SCRIPT_DIR/spark_anti_liquidation_bot/spark_anti_liquidation.py"
+elif [[ "$BOT_NAME" == "uniswap_v3_keeper" ]]; then
+  python3 -u "$SCRIPT_DIR/uniswap_v3_keeper/uniswap_v3_keeper.py"
 else
   echo Bad bot name: "$BOT_NAME"
   exit 1
