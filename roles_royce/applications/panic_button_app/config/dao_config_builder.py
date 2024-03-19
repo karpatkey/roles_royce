88949a82041b97536baa05c0fea1129a11c194c4
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-03-19 12:28:38 +0000

    use pulley for config builder for caching purposes
    - Maybe not necessary if we use knodes

--- roles_royce/applications/panic_button_app/config/dao_config_builder.py
+++ roles_royce/applications/panic_button_app/config/dao_config_builder.py
@@ -1,6 +1,4 @@
-import os
-
-from defabipedia.types import Blockchain, Chain
+from defabipedia.types import Blockchain
 from web3 import Web3
 
 from roles_royce.applications.panic_button_app.config.config_builder import (
@@ -10,6 +8,7 @@ from roles_royce.applications.panic_button_app.config.config_builder import (
     DAOStrategiesBuilder,
     LidoPosition,
 )
+from roles_royce.applications.panic_button_app.pulley_fork import PulleyFork
 
 
 def main(
@@ -19,20 +18,12 @@ def main(
     aura: list[AuraPosition],
     lido: list[LidoPosition],
 ):
-    PUBLIC_ETH_NODE_URL = "https://eth.llamarpc.com"
-    PUBLIC_GC_NODE_URL = "https://rpc.ankr.com/gnosis"
+    with PulleyFork(blockchain) as fork:
+        w3 = Web3(Web3.HTTPProvider(fork.url()))
 
-    w3_eth = Web3(Web3.HTTPProvider(os.environ.get("RR_ETH_FORK_URL", PUBLIC_ETH_NODE_URL)))
-    w3_gc = Web3(Web3.HTTPProvider(os.environ.get("RR_GC_FORK_URL", PUBLIC_GC_NODE_URL)))
-
-    if blockchain == Chain.GNOSIS:
-        w3 = w3_gc
-    elif blockchain == Chain.ETHEREUM:
-        w3 = w3_eth
-
-    strategies = DAOStrategiesBuilder(dao, blockchain, balancer=balancer, aura=aura, lido=lido)
-    strategies_dict = strategies.build_dict(w3)
-    return strategies_dict
+        strategies = DAOStrategiesBuilder(dao, blockchain, balancer=balancer, aura=aura, lido=lido)
+        strategies_dict = strategies.build_dict(w3)
+        return strategies_dict
 
 
 if __name__ == "__main__":
