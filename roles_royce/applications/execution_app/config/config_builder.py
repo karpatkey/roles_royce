1d39645bd875eb12b8a3ce890b5a98f357be95e0
Richymaestro <xxxx@yyy.com>
2024-04-03 13:08:27 +0100

    small fix

--- roles_royce/applications/execution_app/config/config_builder.py
+++ roles_royce/applications/execution_app/config/config_builder.py
@@ -42,10 +42,6 @@ wallet_tokens_swap = [
             },
             {
                 "token_in": ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"],  # USDC
-                "token_out": ["0x6B175474E89094C44Da98b954EedeAC495271d0F"],  # DAI
-            },
-            {
-                "token_in": ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"],  # USDC
                 "token_out": [
                     "0x6B175474E89094C44Da98b954EedeAC495271d0F",  # DAI
                     "0xdAC17F958D2ee523a2206206994597C13D831ec7",
