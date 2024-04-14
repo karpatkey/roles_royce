ee98b90fdc02b4c57b37289a7ab490df820ec42b
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-14 16:25:26 +0100

    http server for execution app

--- roles_royce/applications/execution_app/simulate.py
+++ roles_royce/applications/execution_app/simulate.py
@@ -5,35 +5,23 @@ from roles_royce.applications.execution_app.utils import ENV, start_the_engine
 from roles_royce.toolshed.simulation import TenderlyCredentials, simulate_tx
 
 
-def main():
+def simulate(dao, blockchain, transaction, rpc_url: str | None = None):
     try:
-        parser = argparse.ArgumentParser(
-            description="Script to simulate an exit transaction with Tenderly",
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-
-        parser.add_argument("-d", "--dao", type=str, help="DAO whose funds are to be removed.")
-        parser.add_argument("-b", "--blockchain", type=str, help="Blockchain where the funds are deposited.")
-
-        parser.add_argument("-t", "--transaction", type=str, help="Transaction to simulate, as a json")
-
-        args = parser.parse_args()
-
-        env = ENV(DAO=args.dao, BLOCKCHAIN=args.blockchain)
+        env = ENV(DAO=dao, BLOCKCHAIN=blockchain, local_fork_url=rpc_url)
         tenderly_credentials = TenderlyCredentials(
             account_id=env.TENDERLY_ACCOUNT_ID, project=env.TENDERLY_PROJECT, api_token=env.TENDERLY_API_TOKEN
         )
-        w3, w3_MEV = start_the_engine(env)
+        w3, _ = start_the_engine(env)
         block = w3.eth.block_number
         sim_data = simulate_tx(
-            tx=json.loads(args.transaction),
+            tx=transaction,
             block=block,
             tenderly_credentials=tenderly_credentials,
             sim_type="full",  # TODO: check if 'quick' is enough
             share=True,
         )
 
-        response_message = {
+        return {
             "status": 200,
             "sim_data": {
                 "share_url": sim_data["share_url"],
@@ -42,9 +30,24 @@ def main():
         }
 
     except Exception as e:
-        response_message = {"status": 500, "message": f"Error: {e}"}
+        return {"status": 500, "message": f"Error: {e}"}
 
-    print(json.dumps(response_message))
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script to simulate an exit transaction with Tenderly",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("-d", "--dao", type=str, help="DAO whose funds are to be removed.")
+    parser.add_argument("-b", "--blockchain", type=str, help="Blockchain where the funds are deposited.")
+    parser.add_argument("-t", "--transaction", type=str, help="Transaction to simulate, as a json")
+    args = parser.parse_args()
+
+    transaction = json.loads(args.transaction)
+    response = simulate(dao=args.dao, blockchain=args.blockchain, transaction=transaction)
+
+    print(json.dumps(response))
 
 
 if __name__ == "__main__":
