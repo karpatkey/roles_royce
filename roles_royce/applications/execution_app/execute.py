2b3a2ed9b7de59c6cda2c36729e3e0a92f56ab9b
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-16 12:53:22 +0100

    Update execute.py

--- roles_royce/applications/execution_app/execute.py
+++ roles_royce/applications/execution_app/execute.py
@@ -3,9 +3,8 @@ import json
 
 from web3 import Web3
 
+from roles_royce.applications.execution_app.utils import ENV, start_the_engine
 from roles_royce.roles_modifier import GasStrategies, set_gas_strategy, update_gas_fees_parameters_and_nonce
-
-from .utils import ENV, start_the_engine
 
 
 def execute_env(env, transaction, web3: Web3 | None = None):
@@ -25,6 +24,7 @@ def execute_env(env, transaction, web3: Web3 | None = None):
         else:  # In development environment, send the transaction to the local fork with the unlocked account
             tx_hash = w3.eth.send_transaction(tx)
             w3.eth.wait_for_transaction_receipt(tx_hash)
+            # fork_reset_state(w3, w3.manager.provider.endpoint_uri)
         return {"status": 200, "tx_hash": tx_hash.hex()}
     except Exception as e:
         return {"status": 500, "message": f"Error: {e}"}
