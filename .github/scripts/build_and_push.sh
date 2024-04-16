2bf86d68c24800b11c2dd9bd37d1b0eae5c72ff2
dharmendra kariya <dharamendra.kariya@gmail.com>
2024-04-16 21:45:46 +0530

    minor fix 2

--- .github/scripts/build_and_push.sh
+++ .github/scripts/build_and_push.sh
@@ -30,7 +30,7 @@ DOCKER_TAG_WITH_HASH="${DOCKER_IMAGE_NAME}:${BRANCH_NAME}-${COMMIT_SHA:0:7}"
 mkdir -p kaniko/.docker
 echo "{\"auths\":{\"$DOCKER_REGISTRY\":{\"username\":\"$DOCKER_USERNAME\",\"password\":\"$DOCKER_PASSWORD\"}}}" > kaniko/.docker/config.json
 
-docker run --rm -v $(pwd):/workspace -v $(pwd)/kaniko/.cache:/cache -v $(pwd)kaniko/.docker:/kaniko/.docker \
+docker run --rm -v $(pwd):/workspace -v $(pwd)/kaniko/.cache:/cache -v $(pwd)/kaniko/.docker:/kaniko/.docker \
   gcr.io/kaniko-project/executor:latest \
   --context . \
   --dockerfile "$DOCKERFILE" \
