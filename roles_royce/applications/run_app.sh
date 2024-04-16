2576fd2a42debae84f5d272e66004a188041781a
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-16 12:08:34 +0100

    rename app to execution_app

--- roles_royce/applications/run_app.sh
+++ roles_royce/applications/run_app.sh
@@ -10,7 +10,7 @@ elif [[ "$APP" == "EURe_rebalancing" ]]; then
   python3 -u "$SCRIPT_DIR/EURe_rebalancing_bot/EURe_bot.py"
 elif [[ "$APP" == "GBPe_rebalancing" ]]; then
   python3 -u "$SCRIPT_DIR/GBPe_rebalancing_bot/GBPe_bot.py"
-elif [[ "$APP" == "Execution_http_server" ]]; then
+elif [[ "$APP" == "execution_app" ]]; then
   python3 -u "$SCRIPT_DIR/execution_app/http_server.py"
 elif [[ "$APP" == "spark_anti_liquidation" ]]; then
   python3 -u "$SCRIPT_DIR/spark_anti_liquidation_bot/spark_anti_liquidation.py"
