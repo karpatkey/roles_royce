0916bc26297ac8249301f317a8348c7793d012f9
Ismael Abreu <ismaelga@users.noreply.github.com>
2024-04-22 12:29:17 +0100

    split apps requirements from lib

--- roles_royce/applications/requirements-freeze.sh
+++ roles_royce/applications/requirements-freeze.sh
@@ -5,6 +5,8 @@ if [[ ! $(which pip) =~ "roles_royce" ]]; then
   exit 1
 fi
 
-rm -rf requirements.txt
-pip3 install -e .
+pip3 uninstall -y -r <(pip freeze)
+pip3 install -r requirements-apps.txt
+pip3 install -e ../../.
 pip3 freeze -l | grep -v '-e git' > requirements.txt
+pip3 install -r ../../requirements-dev.txt
