f9b7721a653ca7bdc0a0884d1be71bc90a214817
Richymaestro <xxxx@yyy.com>
2024-05-27 22:33:34 +0100

    get out of sDAI spark

--- roles_royce/toolshed/disassembling/__init__.py
+++ roles_royce/toolshed/disassembling/__init__.py
@@ -4,6 +4,7 @@ from .disassembling_balancer import BalancerDisassembler
 from .disassembling_lido import LidoDisassembler
 from .disassembling_swaps import SwapDisassembler
 from .disassembling_dsr import DSRDisassembler
+from .disassembling_spark import SparkDisassembler
 
 DISASSEMBLERS = {
     "balancer": BalancerDisassembler,
@@ -11,4 +12,5 @@ DISASSEMBLERS = {
     "lido": LidoDisassembler,
     "swaps": SwapDisassembler,
     "dsr": DSRDisassembler,
+    "spark": SparkDisassembler,
 }
