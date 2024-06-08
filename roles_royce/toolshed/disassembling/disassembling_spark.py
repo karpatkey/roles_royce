42d144709c8043fe38aca243a795f9afd7dc3e8d
Richymaestro <xxxx@yyy.com>
2024-06-08 23:13:38 +0100

    add spark sDAI

--- roles_royce/toolshed/disassembling/disassembling_spark.py
+++ roles_royce/toolshed/disassembling/disassembling_spark.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from decimal import Decimal
 from defabipedia.spark import ContractSpecs
-from defabipedia.tokens import EthereumTokenAddr, GnosisTokenAddr
+from defabipedia.tokens import Addresses
 from defabipedia.types import Chain
 from roles_royce.generic_method import Transactable
 from roles_royce.protocols.base import Address
 from roles_royce.protocols.eth import spark
+from roles_royce.protocols import cowswap
 from roles_royce.toolshed.disassembling.disassembling_swaps import SwapDisassembler
 
 from .disassembler import Disassembler, validate_percentage
@@ -42,4 +43,49 @@ class SparkDisassembler(Disassembler):
         txns.append(exit_sdai)
 
         return txns
+    
+    def exit_2(self, percentage: float, exit_arguments: list[dict], amount_to_redeem: int = None) -> list[Transactable]:
+        """
+        Swaps sDAI for USDC. Approves the Cowswap relayer to spend the sDAI if needed, then creates the order using
+        the Cow's order API and creates the sign_order transaction.
+        Args:
+            percentage (float): Percentage of the total sDAI holdings to swap.
+            exit_arguments (list[dict]):  List with one single dictionary with the order parameters from an already
+             created order:
+                arg_dicts = [
+                    {
+                        "max_slippage": 11.25
+                    }
+                ]
+            amount_to_redeem (int, optional): Amount of sDAI to swap. Defaults to None. If None, the 'percentage' of
+                the total sDAI holdings are swapped
+        Returns:
+            list[ Transactable]: List of transactions to execute.
+        """
+
+        max_slippage = exit_arguments[0]["max_slippage"] / 100
+        fraction = validate_percentage(percentage)
+
+        if amount_to_redeem is None:
+            amount_to_redeem = self.get_amount_to_redeem_sdai(fraction)
+
+        if amount_to_redeem == 0:
+            return []
+        
+        if 'anvil' in self.w3.client_version:
+            fork = True
+        else:
+            fork = False
+
+        return cowswap.create_order_and_swap(w3=self.w3,
+                                             avatar=self.avatar_safe_address,
+                                             sell_token=ContractSpecs[self.blockchain].sDAI.address,
+                                             buy_token=Addresses[self.blockchain].USDC,
+                                             amount=amount_to_redeem,
+                                             kind=cowswap.SwapKind.SELL,
+                                             max_slippage=max_slippage,
+                                             valid_duration=20 * 60,
+                                             fork=fork)
+
+    
 
--- tests/toolshed/disassembling/test_disassembling_spark.py
+++ tests/toolshed/disassembling/test_disassembling_spark.py
@@ -1,16 +1,19 @@
 from decimal import Decimal
+import json
 
 from roles_royce.constants import ETHAddr
 from roles_royce.protocols.eth import spark
 from roles_royce.toolshed.protocol_utils.spark.utils import SparkUtils
 from roles_royce.toolshed.disassembling import SparkDisassembler
+from roles_royce.roles_modifier import GasStrategies, set_gas_strategy
 
 from defabipedia.types import Chain
+from defabipedia.spark import ContractSpecs
 
 from tests.roles import apply_presets, deploy_roles, setup_common_roles
 from tests.utils import create_simple_safe, get_balance, steal_token, top_up_address
 from tests.fork_fixtures import accounts
-from tests.fork_fixtures import local_node_eth_replay as local_node_eth
+from tests.fork_fixtures import local_node_eth_replay as local_node_eth, local_node_gc
 
 
 
@@ -70,3 +73,100 @@ def test_integration_1(local_node_eth, accounts):
     dsr_disassembler.send(txn_transactable, private_key=private_key)
 
     assert get_balance(w3, ETHAddr.sDAI, avatar_safe.address) == 461
+
+preset_cowswap = """{
+            "version": "1.0",
+            "chainId": "1",
+            "meta": {
+                "description": "",
+                "txBuilderVersion": "1.8.0"
+            },
+            "createdAt": 1700857590822,
+            "transactions": [
+                {
+                "to": "0x1ffAdc16726dd4F91fF275b4bF50651801B06a86",
+                "data": "0x5e8266950000000000000000000000000000000000000000000000000000000000000004000000000000000000000000E522f854b978650Dc838Ade0e39FbC1417A2FfB0",
+                "value": "0"
+                },
+                {
+                "to": "0x1ffAdc16726dd4F91fF275b4bF50651801B06a86",
+                "data": "0x2fcf52d10000000000000000000000000000000000000000000000000000000000000004000000000000000000000000E522f854b978650Dc838Ade0e39FbC1417A2FfB0569d3489000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002",
+                "value": "0"
+                },
+                {
+                "to": "0x1ffAdc16726dd4F91fF275b4bF50651801B06a86",
+                "data": "0x5e826695000000000000000000000000000000000000000000000000000000000000000400000000000000000000000083F20F44975D03b1b09e64809B757c47f942BEeA",
+                "value": "0"
+                },
+                {
+                "to": "0x1ffAdc16726dd4F91fF275b4bF50651801B06a86",
+                "data": "0x2fcf52d1000000000000000000000000000000000000000000000000000000000000000400000000000000000000000083F20F44975D03b1b09e64809B757c47f942BEeA095ea7b3000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002",
+                "value": "0"
+                }
+            ]
+            }"""
+
+def test_integration_exit_2(local_node_eth, accounts, requests_mock):
+    requests_mock.real_http = True
+    requests_mock.post("https://api.cow.fi/mainnet/api/v1/quote",
+                       text=json.dumps({"quote": {"sellToken": "0x83F20F44975D03b1b09e64809B757c47f942BEeA",
+                                                  "buyToken": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+                                                  "receiver": "0x3e83a898fcbe5d4a0d3e886b287a8359034a5952",
+                                                  "sellAmount": "4498223604396024865",
+                                                  "buyAmount": "5225511848017673212", "validTo": 1712885566,
+                                                  "appData": "{\"appCode\":\"karpatkey_swap\"}",
+                                                  "appDataHash": "0xec4d31696be1272dc6f998e7119a6776e55100c5f8a225ca4ff9529a9eef8e26",
+                                                  "feeAmount": "1776395603475135", "kind": "sell",
+                                                  "partiallyFillable": False, "sellTokenBalance": "erc20",
+                                                  "buyTokenBalance": "erc20", "signingScheme": "presign"},
+                                        "from": "0x3e83a898fcbe5d4a0d3e886b287a8359034a5952",
+                                        "expiration": "2024-04-12T01:04:46.868693450Z", "id": 482569777,
+                                        "verified": False}),
+                       status_code=200)
+    requests_mock.post("https://api.cow.fi/mainnet/api/v1/orders",
+                       text='"0x59dadaec2dd2b84fc499cc62cf95b244701685c9f58491f4f0dd60cfdb9305733e83a898fcbe5d4a0d3e886b287a8359034a595266188cb7"',
+                       status_code=201)
+    w3 = local_node_eth.w3
+    local_node_eth.set_block(20045539)
+    avatar_safe = create_simple_safe(w3=w3, owner=accounts[0])
+    roles_contract = deploy_roles(avatar=avatar_safe.address, w3=w3)
+    setup_common_roles(avatar_safe, roles_contract)
+    apply_presets(
+        avatar_safe,
+        roles_contract,
+        json_data=preset_cowswap,
+        replaces=[("E522f854b978650Dc838Ade0e39FbC1417A2FfB0", "23dA9AdE38E4477b23770DeD512fD37b12381FAB")],
+    )
+
+    blockchain = Chain.get_blockchain_from_web3(w3)
+    steal_token(
+        w3=w3,
+        token=ContractSpecs[blockchain].sDAI.address,
+        holder="0x7d3dF6DC59992463F4776214F93bD70EF6ed11EE",
+        to=avatar_safe.address,
+        amount=8_999_999_999_999_000_000,
+    )
+
+    disassembler_address = accounts[4].address
+    private_key = accounts[4].key
+    role = 4
+
+    spark_disassembler = SparkDisassembler(
+        w3=w3,
+        avatar_safe_address=avatar_safe.address,
+        roles_mod_address=roles_contract.address,
+        role=role,
+        signer_address=disassembler_address,
+    )
+
+    txn_transactable = spark_disassembler.exit_2(percentage=50, exit_arguments=[{'max_slippage': 1}])
+    tx_receipt = spark_disassembler.send(txn_transactable, private_key=private_key)
+
+    assert tx_receipt.status == 1
+    for log in tx_receipt.logs:
+        if log.address == '0x9008D19f58AAbD9eD0D60971565AA8510560ab41':
+            uid_from_tx_log = '0x' + log.data.hex()[194:len(log.data.hex()) - 16]
+            break
+
+    assert uid_from_tx_log == '0xbf949c5371467eae17cc6a1a33fd9a0e670e43f71da22c65886b0f1f89a7bce86fa4080e06fdbd826426746d0121f9947fe7893f66640913'
+
