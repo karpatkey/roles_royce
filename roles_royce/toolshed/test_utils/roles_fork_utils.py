fa699a4a0ca82eaac8e60f6e3d9db5613388ca1a
EktoSAn <san@ektocomms.space>
2024-07-30 22:41:41 -0300

    Use roles contract_methods

--- roles_royce/toolshed/test_utils/roles_fork_utils.py
+++ roles_royce/toolshed/test_utils/roles_fork_utils.py
@@ -3,13 +3,13 @@ import json
 from defabipedia.types import Chain
 from karpatkit.test_utils.fork import TEST_ACCOUNTS, top_up_address
 from karpatkit.test_utils.simple_safe import SimpleSafe
-from karpatkit.test_utils.utils import to_hex_32_bytes
 from web3 import Web3
-from web3.types import TxReceipt
 
 from roles_royce.evm_utils import roles_abi, roles_bytecode
 from roles_royce.generic_method import TxData
 from roles_royce.utils import MULTISENDS, to_checksum_address
+from roles_royce.protocols.roles_modifier.contract_methods import EnableModule, AssignRoles
+from roles_royce.protocols.safe.contract_methods import EnableModule as SafeEnableModule
 
 
 def deploy_roles(w3: Web3, avatar):
@@ -30,10 +30,7 @@ def deploy_roles(w3: Web3, avatar):
 
 def setup_common_roles(safe: SimpleSafe, roles_ctract):
     # set roles_mod as module of safe
-    enable_module_roles = safe.contract.functions.enableModule(roles_ctract.address).build_transaction(
-        {"from": safe.address}
-    )["data"]
-    safe.send(txs=[TxData(contract_address=safe.address, data=enable_module_roles)])
+    safe.send(txs=[SafeEnableModule(safe.address, module=roles_ctract.address)])
 
     # enable an asign roles to the test EOAs
     # EOA           | ROLE N | ROLE NAME
@@ -46,7 +43,6 @@ def setup_common_roles(safe: SimpleSafe, roles_ctract):
     txns = []
     for role_number in range(1, 6):
         account = TEST_ACCOUNTS[role_number]
-        from roles_royce.protocols.roles_modifier.contract_methods import EnableModule, AssignRoles
         txns.extend(
             [
                 EnableModule(roles_mod_address=roles_ctract.address, module=account.address),
@@ -65,31 +61,9 @@ def apply_roles_presets(safe: SimpleSafe, roles_ctract, json_data, replaces=None
         safe.send(txs=[TxData(contract_address=roles_ctract.address, data=data)])
 
 
-def assign_role(local_node, avatar_safe_address: str, roles_mod_address: str, role: int, asignee: str) -> TxReceipt:
-    asignee_32_bytes = to_hex_32_bytes(asignee)
-    role_32_byes = to_hex_32_bytes(role)
-    a = asignee_32_bytes[2:]
-    calldata_to_assign_role = (
-        f"0xa6edf38f"
-        f"{asignee_32_bytes[2:]}"
-        f"0000000000000000000000000000000000000000000000000000000000000060"
-        f"00000000000000000000000000000000000000000000000000000000000000a0"
-        f"0000000000000000000000000000000000000000000000000000000000000001"
-        f"{role_32_byes[2:]}"
-        f"0000000000000000000000000000000000000000000000000000000000000001"
-        f"0000000000000000000000000000000000000000000000000000000000000001"
-    )
-
-    tx_to_assign_role = {
-        "from": avatar_safe_address,
-        "to": roles_mod_address,
-        "data": calldata_to_assign_role,
-        "value": "0",
-    }
-
+def assign_role(local_node, avatar_safe_address: str, roles_mod_address: str, role: int, asignee: str):
     local_node.unlock_account(avatar_safe_address)
     # The amount of ETH of the Avatar address is increased
     top_up_address(local_node.w3, address=avatar_safe_address, amount=1)
-    tx_hash = local_node.w3.eth.send_transaction(tx_to_assign_role)
-    tx_receipt = local_node.w3.eth.wait_for_transaction_receipt(tx_hash, timeout=5)
-    return tx_receipt
+    AssignRoles(roles_mod_address=roles_mod_address, module=asignee, assign_list=[role]).transact(local_node.w3,
+                                                                                                  txparams={"from": avatar_safe_address})
