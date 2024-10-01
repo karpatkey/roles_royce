94aa4d57fe8705c6b5c52603aefec87df3fe64d0
Richymaestro <xxxx@yyy.com>
2024-10-01 12:14:56 +0100

    refactor compound

--- roles_royce/protocols/eth/compound_v3.py
+++ roles_royce/protocols/eth/compound_v3.py
@@ -1,12 +1,9 @@
 import eth_abi
+from defabipedia.compoundv3 import ContractSpecs
+from defabipedia.types import Blockchain, Chain
 
 from roles_royce.constants import ETHAddr, StrEnum
 from roles_royce.protocols.base import Address, AvatarAddress, BaseApproveForToken, ContractMethod
-
-
-class Comet(StrEnum):
-    cUSDCv3 = "0xc3d688B66703497DAA19211EEdff47f25384cdc3"
-    cWETHv3 = "0xA17581A9E3356d9A858b789D68B4d866e593aE94"
 
 
 def _to_hex_pad_64(value):
@@ -17,10 +14,10 @@ ACTION_SUPPLY_NATIVE_TOKEN = _to_hex_pad_64(b"ACTION_SUPPLY_NATIVE_TOKEN")
 ACTION_WITHDRAW_NATIVE_TOKEN = _to_hex_pad_64(b"ACTION_WITHDRAW_NATIVE_TOKEN")
 
 
-class Approve(BaseApproveForToken):
-    def __init__(self, comet: Comet, token: Address, amount: int):
+class ApproveToken(BaseApproveForToken):
+    def __init__(self, comet: Address, token: Address, amount: int):
         super().__init__(token=token, amount=amount)
-        self.args.spender = comet.value
+        self.args.spender = comet
 
 
 class Allow(ContractMethod):
@@ -31,10 +28,11 @@ class Allow(ContractMethod):
 
     name = "allow"
     in_signature = [("manager", "address"), ("is_allowed", "bool")]
-    fixed_arguments = {"manager": ETHAddr.COMPOUND_Bulker, "is_allowed": True}
+    
 
-    def __init__(self, comet: Comet):
-        self.target_address = comet.value
+    def __init__(self, blockchain: Blockchain, comet: Address):
+        self.fixed_arguments = {"manager": ContractSpecs[blockchain].base_bulker.address, "is_allowed": True}
+        self.target_address = comet
         super().__init__()
 
 
@@ -48,27 +46,22 @@ class Supply(ContractMethod):
     name = "supply"
     in_signature = [("asset", "address"), ("amount", "uint256")]
 
-    def __init__(self, comet: Comet, token: Address, amount: int):
+    def __init__(self, comet: Address, token: Address, amount: int):
         super().__init__()
-        self.target_address = comet.value
+        self.target_address = comet
         self.args.asset = token
         self.args.amount = amount
 
-
-class _Invoke(ContractMethod):
+class SupplyETH(ContractMethod):
+    """Supply ETH"""
     name = "invoke"
     in_signature = [("actions", "bytes32[]"), ("data", "bytes[]")]
-    target_address = ETHAddr.COMPOUND_Bulker
-
-
-class SupplyETH(_Invoke):
-    """Supply ETH"""
-
     fixed_arguments = {"actions": [ACTION_SUPPLY_NATIVE_TOKEN]}
 
-    def __init__(self, comet: Comet, avatar: Address, amount: int):
-        super().__init__(value=amount)
-        self.args.data = [eth_abi.encode(types=("address", "address", "uint256"), args=[comet.value, avatar, amount])]
+    def __init__(self, blockchain: Blockchain, comet: Address, avatar: Address, amount: int):
+        super().__init__()
+        self.target_address = ContractSpecs[blockchain].base_bulker.address
+        self.args.data = [eth_abi.encode(types=("address", "address", "uint256"), args=[comet, avatar, amount])]
 
 
 class Withdraw(ContractMethod):
@@ -82,21 +75,24 @@ class Withdraw(ContractMethod):
     name = "withdraw"
     in_signature = [("asset", "address"), ("amount", "uint256")]
 
-    def __init__(self, comet: Comet, token: Address, amount: int):
+    def __init__(self, comet: Address, token: Address, amount: int):
         super().__init__()
-        self.target_address = comet.value
+        self.target_address = comet
         self.args.asset = token
         self.args.amount = amount
 
 
-class WithdrawETH(_Invoke):
+class WithdrawETH(ContractMethod):
     """Withdraw ETH"""
-
+    name = "invoke"
+    in_signature = [("actions", "bytes32[]"), ("data", "bytes[]")]
+    target_address = None
     fixed_arguments = {"actions": [ACTION_WITHDRAW_NATIVE_TOKEN]}
 
-    def __init__(self, comet: Comet, avatar: Address, amount: int):
+    def __init__(self, blockchain: Blockchain, comet: Address, avatar: Address, amount: int):
         super().__init__()
-        self.args.data = [eth_abi.encode(types=("address", "address", "uint256"), args=[comet.value, avatar, amount])]
+        self.target_address = ContractSpecs[blockchain].base_bulker.address
+        self.args.data = [eth_abi.encode(types=("address", "address", "uint256"), args=[comet, avatar, amount])]
 
 
 class Borrow(Withdraw):
@@ -113,9 +109,10 @@ class Claim(ContractMethod):
     name = "claim"
     in_signature = [("comet", "address"), ("src", "address"), ("should_accrue", "bool")]
     fixed_arguments = {"src": AvatarAddress}
-    target_address = ETHAddr.COMPOUND_CometRewards
+    target_address = None
 
-    def __init__(self, comet: Comet, avatar: Address, should_accrue: bool):
+    def __init__(self, blockchain: Blockchain, comet: Address, avatar: Address, should_accrue: bool):
         super().__init__(avatar=avatar)
-        self.args.comet = comet.value
+        self.target_address = ContractSpecs[blockchain].rewards.address
+        self.args.comet = comet
         self.args.should_accrue = should_accrue
